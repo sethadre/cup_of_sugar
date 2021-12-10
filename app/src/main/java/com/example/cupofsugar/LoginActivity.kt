32cf362bf1a99366a98a8cdc474897182b8f7aa0
Larry James <larryjdel@gmail.com>
2021-12-09 16:36:54 -0800

    Fixed folder issue, and return old UI

--- app/src/main/java/com/example/cupofsugar/LoginActivity.kt
+++ app/src/main/java/com/example/cupofsugar/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.example.cupofsugar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
