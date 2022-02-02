2ed70c145e10167295c71cddb012016bcf300298
sethadre <32600946+sethadre@users.noreply.github.com>
2022-02-01 16:04:26 -0800

    buttonClick
    sure

--- app/src/main/java/com/example/cupofsugar/ItemsPageActivity.kt
+++ app/src/main/java/com/example/cupofsugar/ItemsPageActivity.kt
@@ -2,13 +2,19 @@ package com.example.cupofsugar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ItemsPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.items_homepage)
+
+
+
     }
 
-    
+    fun buttonClickedForums(v:View) {
+        
+    }
 
 }
