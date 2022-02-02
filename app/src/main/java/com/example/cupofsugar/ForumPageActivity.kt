16fd92e365e11d86be6a293320f61f1d8219a465
sethadre <32600946+sethadre@users.noreply.github.com>
2022-02-01 17:36:41 -0800

    made some toolbar connections
    items <--> help only

--- app/src/main/java/com/example/cupofsugar/ForumPageActivity.kt
+++ app/src/main/java/com/example/cupofsugar/ForumPageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cupofsugar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,9 +8,39 @@ class ForumPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forums)
+
+        val profileActionButton =
+            findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.profileActionButton)
+        profileActionButton.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val forumsActionButton =
+            findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.forumsActionButton)
+        forumsActionButton.setOnClickListener {
+            val intent = Intent(this, ForumPageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val HomeActionButton =
+            findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.homeActionButton)
+        HomeActionButton.setOnClickListener {
+            val intent = Intent(this, ItemsPageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val helpActionButton =
+            findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.helpActionButton)
+        helpActionButton.setOnClickListener {
+            val intent = Intent(this, HelpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
-
-
-
 
 }
