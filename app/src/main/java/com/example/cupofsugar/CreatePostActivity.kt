075b0d1ddf6eb2c12061945b098086a7c1fd199d
sethadre <32600946+sethadre@users.noreply.github.com>
2022-02-08 17:39:34 -0800

    kotlin background code for create post
    some kotlin code for upload and take photo

--- app/src/main/java/com/example/cupofsugar/CreatePostActivity.kt
+++ app/src/main/java/com/example/cupofsugar/CreatePostActivity.kt
@@ -3,6 +3,7 @@ package com.example.cupofsugar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.items_homepage.*
 
 class CreatePostActivity : AppCompatActivity() {
@@ -16,6 +17,18 @@ class CreatePostActivity : AppCompatActivity() {
             val intent = Intent(this, ItemPostActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        val buttonTakePhoto =
+            findViewById<Button>(R.id.button_take_photo)
+        buttonTakePhoto.setOnClickListener{
+            //placeholder code to ask for camera privileges
+        }
+
+        val buttonUploadPhotos =
+            findViewById<Button>(R.id.button_upload_photo)
+        buttonUploadPhotos.setOnClickListener{
+            //ask for photo
         }
 
     }
