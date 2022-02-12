696034f787ef5391863a16513f59907b49d68b65
Larry James <larryjdel@gmail.com>
2022-02-11 17:49:55 -0800

    database image download

--- app/src/main/java/com/example/cupofsugar/ItemPostActivity.kt
+++ app/src/main/java/com/example/cupofsugar/ItemPostActivity.kt
@@ -3,12 +3,34 @@ package com.example.cupofsugar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory  //for decoding file
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class ItemPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_post)
+        
+        //Start of Back End Stuff
+        val storage = Firebase.storage
+        // Create a reference with an initial file path and name
+        val imagePathReference = storage.reference.child("kirby.jpg")
+        //This is how we download to memory and not as local file
+        val ONE_MEGABYTE: Long = 1024 * 1024
+        // WE LOAD AN IMAGE IN IMAGEVIEW HERE
+            imagePathReference.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+                // Data for "images/kirby.jpg" is returned, use this as needed
+                val bitmap = BitmapFactory.decodeByteArray(it,0,it.size)
+                findViewById<ImageView>(R.id.postImg).setImageBitmap(bitmap)
+        }.addOnFailureListener {
+            //Make Toast incase image/post didn't exist
+        }
+        //End Back End Stuff
+
 
         val cancelButton =
             findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.cancelActionButton)
