0a4747ec68497c2d318141465529b1627b646e50
Nima <siahnima@gmail.com>
2022-02-11 19:20:42 -0800

    fragment stuff

--- app/src/main/java/com/example/cupofsugar/HelpActivity.kt
+++ app/src/main/java/com/example/cupofsugar/HelpActivity.kt
@@ -2,12 +2,20 @@ package com.example.cupofsugar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
-    }
 
+        val supportActionButton = findViewById<Button>(R.id.supportActionButton)
+        supportActionButton.setOnClickListener {
+            val intent = Intent(this, SupportActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
 }
--- app/src/main/java/com/example/cupofsugar/SupportActivity.kt
+++ app/src/main/java/com/example/cupofsugar/SupportActivity.kt
@@ -2,10 +2,18 @@ package com.example.cupofsugar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentManager
 
 class SupportActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support)
+
+        //add fragment
+        if (findViewById<FrameLayout>(R.id.frameLayout) != null)
+            supportFragmentManager.beginTransaction().add(R.id.frameLayout, TopicFragment()).commit()
     }
 }
