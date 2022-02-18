2299231389cf0f86a9be641f712d108d87d10137
sethadre <sethadre@yahoo.com>
2022-02-18 12:59:44 -0800

    profile kt and xml changes
    XML: moved top toolbar code down, so now the scroll view doesn't scroll on top
    KT: added four image views and they're clickable but... theyre just snack bars rn, need to link to post info

--- app/src/main/java/com/example/cupofsugar/ProfileActivity.kt
+++ app/src/main/java/com/example/cupofsugar/ProfileActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
@@ -23,12 +24,61 @@ class ProfileActivity : AppCompatActivity() {
         // WE LOAD AN IMAGE IN IMAGEVIEW HERE
         imagePathReference.getBytes(ONE_MEGABYTE).addOnSuccessListener {
             // Data for "images/kirby.jpg" is returned, use this as needed
-            val bitmap = BitmapFactory.decodeByteArray(it,0,it.size) //it is like "this" .getBytes is a task byte array is parameter
-            findViewById<ImageView>(R.id.imageView7).setImageBitmap(bitmap)
+            //it is like "this" .getBytes is a task byte array is parameter
+            val bitmap = BitmapFactory.decodeByteArray(it,0,it.size)
+            findViewById<ImageView>(R.id.profileImageView).setImageBitmap(bitmap)
         }.addOnFailureListener {
             //Make Toast incase image/post didn't exist
         }
         //End Back End Stuff
+
+        val imageClick9 = findViewById<ImageView>(R.id.imageView9)
+        val imageClick10 = findViewById<ImageView>(R.id.imageView10)
+        val imageClick11 = findViewById<ImageView>(R.id.imageView11)
+        val imageClick12 = findViewById<ImageView>(R.id.imageView12)
+
+
+        val postImagePathReference9 = storage.reference.child("postImages/post0/cake.jpg")
+        postImagePathReference9.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+            val bitmap1 = BitmapFactory.decodeByteArray(it,0,it.size)
+            findViewById<ImageView>(R.id.imageView9).setImageBitmap(bitmap1)
+
+        }
+
+        val postImagePathReference10 = storage.reference.child("postImages/post0/candy.jpg")
+        postImagePathReference10.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+            val bitmap2 = BitmapFactory.decodeByteArray(it,0,it.size)
+            findViewById<ImageView>(R.id.imageView10).setImageBitmap(bitmap2)
+        }
+
+        val postImagePathReference11 = storage.reference.child("postImages/post0/tomato.jpg")
+        postImagePathReference11.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+            val bitmap3 = BitmapFactory.decodeByteArray(it,0,it.size)
+            findViewById<ImageView>(R.id.imageView11).setImageBitmap(bitmap3)
+        }
+
+        val postImagePathReference12 = storage.reference.child("postImages/post0/cake.jpg")
+        postImagePathReference12.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+            val bitmap4 = BitmapFactory.decodeByteArray(it,0,it.size)
+            findViewById<ImageView>(R.id.imageView12).setImageBitmap(bitmap4)
+        }
+
+
+        imageClick9.setOnClickListener {
+            //Right now theyre toast messages,
+            //Need to replace the toasts with codes to open the post info
+            Toast.makeText(this,"Image clicked, I lied",Toast.LENGTH_SHORT).show()
+        }
+        imageClick10.setOnClickListener {
+            Toast.makeText(this,"Image clicked, such Lollipop",Toast.LENGTH_SHORT).show()
+        }
+        imageClick11.setOnClickListener {
+            Toast.makeText(this,"Image clicked, MAXIMUM TOMATER",Toast.LENGTH_SHORT).show()
+        }
+        imageClick12.setOnClickListener {
+            Toast.makeText(this,"Image clicked, the cake is a lie",Toast.LENGTH_SHORT).show()
+        }
+
 
         val profileActionButton =
             findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.profileActionButton)
