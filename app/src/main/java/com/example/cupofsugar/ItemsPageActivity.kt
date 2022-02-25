872c83bfb51a90d44530a1598fcbd0ac0d704aab
sethadre <sethadre@yahoo.com>
2022-02-24 17:38:01 -0800

    changes to items homepage and the xml
    commented out hardcoded stuff, trying other stuff

--- app/src/main/java/com/example/cupofsugar/ItemsPageActivity.kt
+++ app/src/main/java/com/example/cupofsugar/ItemsPageActivity.kt
@@ -2,19 +2,20 @@ package com.example.cupofsugar
 
 import android.content.Intent
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Display
 import android.view.View
-import android.widget.Button
 import android.widget.GridView
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.items_homepage.*
+
 
 class ItemsPageActivity : AppCompatActivity() {
 
@@ -42,71 +43,28 @@ class ItemsPageActivity : AppCompatActivity() {
         //This is how we download to memory and not as local file
         val ONE_MEGABYTE: Long = 1024 * 1024
 
-        //HARDCODED MANUALLY GRABBED
-        //TODO : Array? or something to autoload images based on tags or distance
-        val postPath1   = storage.reference.child("postImages/post0/laptop.jpg")
-        val postPath2   = storage.reference.child("postImages/post0/cake.jpg")
-        val postPath3   = storage.reference.child("postImages/post0/candy.jpg")
-        val postPath4   = storage.reference.child("postImages/post0/coffee.jpeg")
-        val postPath5   = storage.reference.child("postImages/post0/monster.jpg")
-        val postPath6   = storage.reference.child("postImages/post0/sugar.jpeg")
-        val postPath7   = storage.reference.child("postImages/post0/sweater.jpeg")
-        val postPath8   = storage.reference.child("postImages/post0/laptop.jpg")
-        val postPath9   = storage.reference.child("postImages/post0/tomato.jpg")
-        val postPath10  = storage.reference.child("postImages/post0/cake.jpg")
-        val postPath11  = storage.reference.child("postImages/post0/sugar.jpeg")
-        val postPath12  = storage.reference.child("postImages/post0/candy.jpg")
-//
-        //Also hard coded clicks
-        //TODO: automate it
-        val postClick1  = findViewById<ImageView>(R.id.postView1)
-        val postClick2  = findViewById<ImageView>(R.id.postView2)
-        val postClick3  = findViewById<ImageView>(R.id.postView3)
-        val postClick4  = findViewById<ImageView>(R.id.postView4)
-        val postClick5  = findViewById<ImageView>(R.id.postView5)
-        val postClick6  = findViewById<ImageView>(R.id.postView6)
-        val postClick7  = findViewById<ImageView>(R.id.postView7)
-        val postClick8  = findViewById<ImageView>(R.id.postView8)
-        val postClick9  = findViewById<ImageView>(R.id.postView9)
-        val postClick10 = findViewById<ImageView>(R.id.postView10)
-        val postClick11 = findViewById<ImageView>(R.id.postView11)
-        val postClick12 = findViewById<ImageView>(R.id.postView12)
-//
+        val iv1 = findViewById<ImageView>(R.id.postView1)
+        val iv2 = findViewById<ImageView>(R.id.postView2)
+        val iv3 = findViewById<ImageView>(R.id.postView3)
 
-        postPath1.getBytes(ONE_MEGABYTE).addOnSuccessListener {
-            val bitmap1 = BitmapFactory.decodeByteArray(it,0,it.size)
-            findViewById<ImageView>(R.id.postView1).setImageBitmap(bitmap1)
-        }
-        postPath2.getBytes(ONE_MEGABYTE).addOnSuccessListener {
-            val bitmap2 = BitmapFactory.decodeByteArray(it,0,it.size)
-            findViewById<ImageView>(R.id.postView2).setImageBitmap(bitmap2)
-        }
-        postPath3.getBytes(ONE_MEGABYTE).addOnSuccessListener {
-            val bitmap3 = BitmapFactory.decodeByteArray(it,0,it.size)
-            findViewById<ImageView>(R.id.postView3).setImageBitmap(bitmap3)
-        }
+        //Image View array
+        val IMGS = arrayListOf<ImageView>(iv1, iv2, iv3)
+        //IMGS += iv4
+        //adds iv4 to IMGS array
 
-//        imageClick9.setOnClickListener {
-//            //Right now theyre toast messages,
-//            //Need to replace the toasts with codes to open the post info
-//            Toast.makeText(this,"Image clicked, I lied", Toast.LENGTH_SHORT).show()
-//        }
-//        imageClick10.setOnClickListener {
-//            Toast.makeText(this,"Image clicked, such Lollipop", Toast.LENGTH_SHORT).show()
-//        }
-//        imageClick11.setOnClickListener {
-//            Toast.makeText(this,"Image clicked, MAXIMUM TOMATER", Toast.LENGTH_SHORT).show()
-//        }
-//        imageClick12.setOnClickListener {
-//            Toast.makeText(this,"Image clicked, the cake is a lie", Toast.LENGTH_SHORT).show()
-//        }
+        //TODO GRAB STUFF FROM DB
 
+
+        //this pulls the email from the database
 
         //Firebase declare
         db= FirebaseFirestore.getInstance()
         auth= FirebaseAuth.getInstance()
         val user = auth.currentUser
         val docRef = db.collection("Users").document(user?.uid.toString())
+        val itemRef = db.collection("Items").document("ufFoRA4axiYE5oQ7eLDnFqKl8bA3flowers 2")
+
+
         docRef.get().addOnSuccessListener { document ->
             if (document != null) {
                 Log.d(TAG, "DocumentSnapshot data: ${document.data}")
@@ -125,13 +83,7 @@ class ItemsPageActivity : AppCompatActivity() {
 
         setContentView(R.layout.items_homepage) //moved this line lower
 
-//        val postActionButton =
-//            findViewById<Button>(R.id.goToPostButton)
-//        postActionButton.setOnClickListener {
-//            val intent = Intent(this, ItemsPageActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }
+
         val createPostActionButton =
             findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.createPostActionButton)
         createPostActionButton.setOnClickListener {
@@ -139,6 +91,11 @@ class ItemsPageActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+
+        // BOTTOM BAR BUTTONS
+        // ****************************************************************************************
+        // ****************************************************************************************
         val profileActionButton =
             findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.profileActionButton)
         profileActionButton.setOnClickListener {
@@ -146,7 +103,6 @@ class ItemsPageActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
         val forumsActionButton =
             findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.forumsActionButton)
         forumsActionButton.setOnClickListener {
@@ -155,13 +111,15 @@ class ItemsPageActivity : AppCompatActivity() {
             finish()
         }
 
-        val HomeActionButton =
-            findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.homeActionButton)
-        HomeActionButton.setOnClickListener {
-            val intent = Intent(this, ItemsPageActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        //We are home we dont need functionality
+
+//        val HomeActionButton =
+//            findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.homeActionButton)
+//        HomeActionButton.setOnClickListener {
+//            val intent = Intent(this, ItemsPageActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
 
         val helpActionButton =
             findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.helpActionButton)
@@ -178,13 +136,10 @@ class ItemsPageActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-        val createButton =
-            findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.createActionButton)
-        createButton.setOnClickListener {
-            val intent = Intent(this, CreatePostActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        // ****************************************************************************************
+        // ****************************************************************************************
+        // END BOTTOM BAR BUTTONS
+
     }
 
     //firebase stuff
@@ -207,6 +162,8 @@ class ItemsPageActivity : AppCompatActivity() {
     }
     //end of firebase stuff
 
+
+
     fun logout(view: View){
         val intent= Intent(this,LoginActivity::class.java)
         startActivity(intent)
@@ -223,3 +180,61 @@ class ItemsPageActivity : AppCompatActivity() {
     }
 
 }
+
+// ****************************                                         ***************************
+// ****************************   OLD CODE KEEP FOR ADRIAN's REFERENCE  ***************************
+// ****************************                                         ***************************
+
+//HARDCODED MANUALLY GRABBED
+//TODO : Array? or something to autoload images based on tags or distance
+//        val postPath1   = storage.reference.child("postImages/post0/tomato.jpg")
+//        val postPath2   = storage.reference.child("postImages/post0/cake.jpg")
+//        val postPath3   = storage.reference.child("postImages/post0/candy.jpg")
+//
+//        postPath1.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+//            val bitmap1 = BitmapFactory.decodeByteArray(it,0,it.size)
+//            findViewById<ImageView>(R.id.postView1).setImageBitmap(bitmap1)
+//        }
+//        postPath2.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+//            val bitmap2 = BitmapFactory.decodeByteArray(it,0,it.size)
+//            findViewById<ImageView>(R.id.postView2).setImageBitmap(bitmap2)
+//        }
+//        postPath3.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+//            val bitmap3 = BitmapFactory.decodeByteArray(it,0,it.size)
+//            findViewById<ImageView>(R.id.postView3).setImageBitmap(bitmap3)
+//        }
+
+
+//Also hard coded clicks
+//TODO: automate it
+
+//        val postClick1  = findViewById<ImageView>(R.id.postView1)
+//        val postClick2  = findViewById<ImageView>(R.id.postView2)
+//        val postClick3  = findViewById<ImageView>(R.id.postView3)
+//
+//        postClick1.setOnClickListener {
+//            Toast.makeText(this,"Image 1 Clicked",Toast.LENGTH_SHORT).show()
+//        }
+//        postClick2.setOnClickListener {
+//            Toast.makeText(this,"Image 1 Clicked",Toast.LENGTH_SHORT).show()
+//        }
+//        postClick3.setOnClickListener {
+//            Toast.makeText(this,"Image 1 Clicked",Toast.LENGTH_SHORT).show()
+//        }
+
+
+
+//        imageClick9.setOnClickListener {
+//            //Right now theyre toast messages,
+//            //Need to replace the toasts with codes to open the post info
+//            Toast.makeText(this,"Image clicked, I lied", Toast.LENGTH_SHORT).show()
+//        }
+//        imageClick10.setOnClickListener {
+//            Toast.makeText(this,"Image clicked, such Lollipop", Toast.LENGTH_SHORT).show()
+//        }
+//        imageClick11.setOnClickListener {
+//            Toast.makeText(this,"Image clicked, MAXIMUM TOMATER", Toast.LENGTH_SHORT).show()
+//        }
+//        imageClick12.setOnClickListener {
+//            Toast.makeText(this,"Image clicked, the cake is a lie", Toast.LENGTH_SHORT).show()
+//        }
