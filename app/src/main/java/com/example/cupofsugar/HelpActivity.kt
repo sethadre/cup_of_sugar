701c25400307d3e281d7f089a9b848714d2b92be
nimasiah <siahnima@gmail.com>
2022-03-15 03:10:37 -0700

    ad, resource, and help page stuff

--- app/src/main/java/com/example/cupofsugar/HelpActivity.kt
+++ app/src/main/java/com/example/cupofsugar/HelpActivity.kt
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.util.Log
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class HelpActivity : AppCompatActivity() {
+
+    private var mInterstitialAd: InterstitialAd? = null
+    private var TAG = "HelpActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+        initAd()
 
         val supportActionButton = findViewById<Button>(R.id.supportButton)
         supportActionButton.setOnClickListener {
@@ -17,9 +26,9 @@ class HelpActivity : AppCompatActivity() {
             finish()
         }
 
-        val helpCupOfSugarActionButton = findViewById<Button>(R.id.hcosButton)
-        helpCupOfSugarActionButton.setOnClickListener {
-            val intent = Intent(this, hcosActivity::class.java)
+        val resourceActionButton = findViewById<Button>(R.id.resourcesButton)
+        resourceActionButton.setOnClickListener {
+            val intent = Intent(this, ResourceActivity::class.java)
             startActivity(intent)
             finish()
         }
@@ -62,6 +71,46 @@ class HelpActivity : AppCompatActivity() {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+    private fun initAd(){
+        MobileAds.initialize(this) {}
+        var adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/8691691433", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.message)
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+                mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+                    override fun onAdDismissedFullScreenContent() {
+                        Log.d(TAG, "Ad was dismissed.")
+                        initAd()
+                        super.onAdDismissedFullScreenContent()
+                    }
+
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                        Log.d(TAG, "Ad failed to show.")
+                    }
+
+                    override fun onAdShowedFullScreenContent() {
+                        Log.d(TAG, "Ad showed fullscreen content.")
+                        mInterstitialAd = null
+                    }
+                }
+            }
+        })
+    }
+
+    fun show(view: android.view.View) {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
         }
     }
 }
